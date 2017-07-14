3f69b1e9cfe39e195dc273e554fccfa5f3bd9956
vkumar <vkumar@barefootnetworks.com>
2017-07-14 11:36:54 -0700

    update

--- 1.c
+++ 1.c
@@ -10,12 +10,12 @@ int f(void) {
 int main() { 
   int x = 0x12345678;
   char *p = (char *)&x;
-  printf( "%x\n", *p);
-  printf( "%x\n", *p + 1);
-  printf( "%x\n", *( p + 1));
+  printf( "%x\n", *p); // 78
+  printf( "%x\n", *p + 1); // 79
+  printf( "%x\n", *( p + 1)); //56
 
-  printf( "%x\n", *(char*)&x);
-  printf( "%x\n", *(char*)(&x+1));
+  printf( "%x\n", *(char*)&x); // 78
+  printf( "%x\n", *(char*)(&x+1)); // junk
 
   printf("%d %d\n", f(), f());
 
